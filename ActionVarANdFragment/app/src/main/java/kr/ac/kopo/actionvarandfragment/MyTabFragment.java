5b3132f652b21e38d50d162cad92bbc680a4029e
janrendel <janrendel11@gmail.com>
2025-06-09 15:10:35 +0900

    옵션 메뉴

---
+++ ActionVarANdFragment/app/src/main/java/kr/ac/kopo/actionvarandfragment/MyTabFragment.java
@@ -0,0 +1,51 @@
+package kr.ac.kopo.actionvarandfragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyTabFragment extends Fragment {
+    String tabName;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle data = getArguments();
+        tabName = data.getString("tabName");
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LinearLayout baseLayout = new LinearLayout(super.getActivity());
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        baseLayout.setLayoutParams(params);
+
+        ImageView img1 = new ImageView(super.getActivity());
+        img1.setImageResource(R.drawable.spring);
+        img1.setAdjustViewBounds(true);
+
+        baseLayout.addView(img1);
+
+        if (tabName.equals("Sping"))
+            baseLayout.setBackgroundColor(Color.GREEN);
+        img1.setImageResource(R.drawable.spring);
+        if (tabName.equals("Summer"))
+            baseLayout.setBackgroundColor(Color.RED);
+        img1.setImageResource(R.drawable.summer);
+        if (tabName.equals("Fall"))
+            baseLayout.setBackgroundColor(Color.rgb(41, 28, 19));
+        img1.setImageResource(R.drawable.fall);
+
+        return baseLayout;
+    }
+}
